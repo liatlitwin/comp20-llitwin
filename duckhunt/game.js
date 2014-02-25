a4f86d82fed0b12c41b1afb6f8996c080ceb08c9
liat litwin <liat.litwin@gmail.com>
2014-02-25 12:05:57 -0500

    Deleted line in index.html that was giving an error in html validation

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,5 +1,4 @@
-
-    function draw() {
+function draw() {
         canvas = document.getElementById('game');
        
         // Check if canvas is supported on browser
@@ -13,6 +12,7 @@
             img.src = 'assets/duckhunt.png';
             img.onload = function fromSprite(){
                 
+                //draw tree, dog, bushes
                 ctx.drawImage(img, 0,271, 84,125,20,20,352,525);
                 ctx.drawImage(img, 0,700,800,183,0, 417, 800,183);
                 ctx.drawImage(img, 0,0,61,52,300,510,91,78);
@@ -20,7 +20,6 @@
                 //draw birds:
                 ctx.drawImage(img, 168, 153, 40,40,500,200,60,60);
                 ctx.drawImage(img, 339, 119, 35,35,30,190,60,60);
-
                 ctx.translate(700, 20);
                 ctx.scale(-1, 1);
                 ctx.drawImage(img, 258, 119, 37,37,0,0,60,60);
